aa454f3a245f56e3cc5a7d645400ad0272a9f846
Александр <alexpavlich@yandex.ru>
2022-03-28 22:03:56 +0300

    задание 3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -13,17 +13,39 @@ public class Main {
         char g = 33;
         boolean muIsChampion = mu > 1;
         System.out.println (g);
-        System.out.println (" Manchester United is a Champion? " +  muIsChampion);
+        System.out.println ("Manchester United is a Champion? " +  muIsChampion);
 
         // Homework 2
         double firstBoxer = 78.2;
         double secondBoxer = 82.7;
         double weightOfAllBoxers = firstBoxer + secondBoxer;
-        double weightDifference = secondBoxer - firstBoxer;
+        double weightDifference = firstBoxer - secondBoxer;
         System.out.println("Total weight of boxers = " + weightOfAllBoxers + "kg");
         System.out.println ("The difference in weight is " + weightDifference + "kg");
 
-        double first = 78.2;
+        // Homework 3
+        byte bananas = 5;
+        short milk = 200;
+        short icecream = 2;
+        short eggs = 4;
+        int weightbananas = bananas * 80;
+        int weightmilk = milk/100 * 105;
+        int weighticecream = icecream * 100;
+        int weighteggs = eggs * 70;
+        float weightbreakfastg = weightbananas + weighteggs + weighticecream + weightmilk;
+        float weightbreakfastkg = weightbreakfastg / 1000;
+        System.out.println ("Weight bananas = " + weightbananas + "g");
+        System.out.println ("Weight milk = " + weightmilk +  "g");
+        System.out.println ("Weight icecream = " + weighticecream +  "g");
+        System.out.println ("Weight eggs = " + weighteggs +  "g");
+        System.out.println ("Breakfast weight in grams = " + weightbreakfastg +  "g");
+        System.out.println ("Breakfast weight in kilograms = " + weightbreakfastkg +  "kg");
+
+        // Homework 4
+
+
+
+
 
     }
 }
