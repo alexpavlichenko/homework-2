982a5e530b9f4668e79ea71735a177b1d21ebc9e
Alexpavlichenko <alexpavlich@yandex.ru>
2022-03-28 17:56:45 +0300

    выполнил дз 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,8 +3,33 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Пишем наше дз");
-        System.out.println("Делаем коммит и пуш");
-        System.out.println("Переходим в гитхаб");
+        // Homework 1
+        byte mu = 2;
+        short mc = 3;
+        int chelsea = 4;
+        long liverpool = 5;
+        float goalsScoredPerGameOnAverage = 95.5f;
+        double goalsConcededPerGameOnAverage = 46.5;
+        char g = 33;
+        boolean muIsChampion = mu > 1;
+        System.out.println (g);
+        System.out.println (" Manchester United is a Champion? " +  muIsChampion);
+
+        // Homework 2
+        double firstBoxer = 78.2;
+        double secondBoxer = 82.7;
+        double weightOfAllBoxers = firstBoxer + secondBoxer;
+        double weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Total weight of boxers = " + weightOfAllBoxers + "kg");
+        System.out.println ("The difference in weight is " + weightDifference + "kg");
+
+
+
+
+
+
+
+
+
     }
 }
